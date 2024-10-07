331e950fb65f154664e4f3118a57e086fe9270a1
abdullahhbutt5 <abdullahbutteng@gmail.com>
2024-10-07 17:41:35 +0000

    DataType-Coversion

---
+++ 1/DataType-Conversion.js
@@ -0,0 +1,38 @@
+// let babarScore="32"
+// console.log(babarScore);                     // Output=> 32
+// console.log(typeof babarScore);              // Type=>string
+
+// let bobzyScore=Number(babarScore)        
+// console.log(bobzyScore);                     // Output=> 32
+// console.log(typeof bobzyScore);              // Type=> number
+
+// let babarScore="123abd123"
+// console.log(babarScore);                     // Output=> 123abd123
+// console.log(typeof babarScore);              // Type=> string
+
+// let bobzyScore=Number(babarScore)           
+// console.log(bobzyScore);                     // Output=> NaN
+// console.log(typeof bobzyScore);              // Type=> number
+
+
+// let babarScore=32
+// console.log(babarScore);                  // Output=> 32
+// console.log(typeof babarScore);           // Type=> number
+
+// let bobzyScore=String(babarScore)
+// console.log(bobzyScore);                  // Output=> 32
+// console.log(typeof bobzyScore);           // Type=> string
+
+let babarScore=32
+console.log(babarScore);                  // Output=> 32
+console.log(typeof babarScore);           // Type=> number
+
+let bobzyScore=Boolean(babarScore)
+console.log(bobzyScore);                  // Output=> true
+console.log(typeof bobzyScore);           // Type=> boolean
+
+
+// >0  => true
+//  0  => false
+// "a" => true
+// ""  => false
