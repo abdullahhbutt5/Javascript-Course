281070816301de00226c1be472324253cb35e121
abdullahhbutt5 <abdullahbutteng@gmail.com>
2024-10-10 17:04:55 +0500

    Add files via upload

---
+++ 1/7_DataTypes.js
@@ -0,0 +1,26 @@
+"use strict";          // Treat all code as newer version
+
+//alert("hello")       // We are using node.js not browser
+
+console.log("abd"); console.log(3+3);  // Code readability should be high
+
+let name="abdullah"
+let age=20
+let isLoggedIn=true
+
+// Some useful Data types
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+console.log(typeof"abd");             // Type is string
+console.log(typeof age);              // Type is number
+console.log(typeof name);             // Type is string
+console.log(typeof isLoggedIn);       // Type is boolean
+console.log(typeof null);             // Type is object
+console.log(typeof undefined);        // Type is undefined
