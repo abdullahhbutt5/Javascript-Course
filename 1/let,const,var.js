a149c9fcf10c40258408ccc45a240be41cf02f98
abdullahhbutt5 <abdullahbutteng@gmail.com>
2024-10-10 11:23:06 +0000

    let,const,var done

---
+++ 1/let,const,var.js
@@ -0,0 +1,8 @@
+const accountId=5544332211
+console.log(accountId);
+let name='Abdullah'
+console.log(name);
+var city="Gujranwala"                     // Do not use var
+console.log(city);
+
+console.table([accountId,name,city])      // Output in table form
