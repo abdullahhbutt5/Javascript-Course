8ad525d0d469987ffedd73f3b2a1d72af56f7bfc
abdullahhbutt5 <abdullahbutteng@gmail.com>
2024-10-22 13:40:20 +0000

    functions done

--- 3/1_functions.js
+++ 3/1_functions.js
@@ -8,12 +8,12 @@
 
 function addTwoNum(number1,number2) {
     let result=number1+number2
-    return result                             // => If not returned output of "console.log(result)" will be undefined
+    return result                            // => If not returned output of "console.log(result)" will be undefined
     // return number1+number2
 }
 // console.log(addTwoNum(7,1))
 const result=addTwoNum(7,1)
-console.log("Result: ",result);
+// console.log("Result: ",result);
 
 
 function userLogin(username='abd') {
@@ -24,5 +24,34 @@ function userLogin(username='abd') {
     return `${username} just logged in`
 }
 // console.log(userLogin())
-console.log(userLogin('Abd'))                // Overwrites the value given as parameter
-console.log(userLogin('Butt'))
+// console.log(userLogin('Abd'))             // Overwrites the value given as parameter
+// console.log(userLogin('Butt'))
+
+
+function calculatePrice(num,...price) {      // (...price => Rest)
+    return price
+}
+// console.log(calculatePrice(200,300,400,500,600))
+
+const user={
+    username:"Abdullah",
+    price:399
+}
+// console.log(typeof user);
+function handleObject(obj){
+    console.log(`Username is ${obj.username} and price is ${obj.price}`);   
+}
+// handleObject(user)                        // Calls the object created above
+handleObject({                               // Direct object 
+    username:"Butt",
+    price:299
+})
+
+// **************************Array********************************
+// const arr=[200,300,400]
+function getSecond(getsec) {
+    return getsec[1]
+}
+// console.log(getSecond(arr));               // Calls the Array created above
+console.log(getSecond([200,300,400]));        // Direct Array
+
