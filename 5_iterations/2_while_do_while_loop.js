0702dee44ce7623b93b3b5c0a99a677319dae398
abdullahhbutt5 <abdullahbutteng@gmail.com>
2024-10-30 10:52:18 +0000

    while loop done

---
+++ 5_iterations/2_while_do_while_loop.js
@@ -0,0 +1,23 @@
+// While Loop
+// let i=0;
+// while (i<=10) {
+//     console.log(`Value of i is: ${i}`);
+//     i++                                 // Returns error if "i" is declared with "const" coz const remains same
+// }
+
+
+// While loop with array
+// let myArray=['Iron man','Superman','Batman']
+// let i=0
+// while (i<myArray.length) {
+//     console.log(`Super hero is: ${myArray[i]}`);
+//     i++
+// }
+
+
+// let score=11
+// do{                                        // It will always execute once no matter what is the condition
+//     console.log(`Score is: ${score}`);
+//     score++
+// }
+//     while(score<=10)
